23189b6a63dedc7ea86048be34981b9c5e6d61aa
hoollyzhang <hoollyzhang@tencent.com>
2023-07-27 13:24:02 +0800

    feat: 修改脚本

--- changeName.sh
+++ changeName.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+# 检查输入参数
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <new_project_name>"
+    echo "用法: $0 <new_project_name>"
     exit 1
 fi
 
-# 输入你的新项目名
-NEW_PROJECT_NAME=$1
+# 定义要替换的原始字符串
+original_string="flutter_template"
 
-# 将 "flutter_template" 替换为新的项目名
-find . -type f \( -name "*.dart" -o -name "*.yaml" -o -name "*.json" -o -name "README.md" \) -exec sed -i "s/flutter_template/$NEW_PROJECT_NAME/g" {} +
+# 定义要替换成的新字符串
+new_string="$1"
 
-# 打印替换完成的信息
-echo "Project renamed to $NEW_PROJECT_NAME"
+# 定义要搜索的文件类型
+file_types=("*.dart" "*.json" "*.yaml")
+
+# 使用循环遍历文件类型
+for file_type in "${file_types[@]}"; do
+    # 使用find命令查找所有匹配的文件，并对每个文件进行替换操作
+    find . -name "$file_type" -exec sed -i '' -e "s/${original_string}/${new_string}/g" {} \;
+done
+
+echo "替换完成！"
